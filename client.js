1fa58c3473467e06ca3005761fa866cd62fa780a
StaceyAlexMiller90 <staceyalexmiller@gmail.com>
2020-02-22 01:07:41 +0100

    added Heroku URL

--- client.js
+++ client.js
@@ -2,7 +2,7 @@ const axios = require('axios')
 
 const day = process.argv[2]
 
-const url = `xxxxxxx${day}`
+const url = `https://study-guide-codaisseur.herokuapp.com/${day}`
 
 const logSummary = async (url) => {
   await axios.get(url)
